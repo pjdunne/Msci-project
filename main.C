7c9860a4bb905cd510d61e1ed5afe3d4c5b099d2
priyanka-sadhwani <32648190+priyanka-sadhwani@users.noreply.github.com>
2018-02-02 14:42:23 +0000

    Update main.C

--- main.C
+++ main.C
@@ -9,7 +9,7 @@ float pz[24];
 float energy[24];
 int mode;
 float Enu_t;
-float costhetalep;
+float coslep;
 float Enu_r;
 float E_diff;
 
@@ -20,10 +20,12 @@ tree->SetBranchAddress("py",&py);
 tree->SetBranchAddress("pz",&pz);
 tree->SetBranchAddress("E",&energy);
 tree->SetBranchAddress("Mode",&mode);
-tree->SetBranchAddress("Enu_QE",&Enu_t);
-tree->SetBranchAddress("CosLep",&costhetalep);
+//tree->SetBranchAddress("",&Enu_t);  Edit when new tree given
+tree->SetBranchAddress("CosLep",&coslep);
 
 for(unsigned iEntry=0;iEntry<tree->GetEntries();iEntry++){
   tree->GetEntry(iEntry);
   for(int iVecEntry=0;iVecEntry<nfsp;iVecEntry++){
     Particle a = new Particle(pdg[iVecEntry],px[iVecEntry],py[iVecEntry],pz[iVecEntry],energy[iVecEntry]);
+  }
+}
