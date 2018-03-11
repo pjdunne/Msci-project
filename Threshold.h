592b0ab045338474ee35660c2820fd8738a20b22
alastair95 <32539075+alastair95@users.noreply.github.com>
2018-03-11 22:04:31 +0000

    Update Threshold.h

--- Threshold.h
+++ Threshold.h
@@ -6,6 +6,6 @@ class Threshold: public TObject
        
 
         Threshold();  
-        vector<Particle> ThreshFunc(vector<Particle> PartVec, float th);
+        vector<Particle> ThreshFunc(vector<Particle> PartVec, float prot_thresh, float neutron_thresh, float pi_pm_thresh, float pi0_thresh, float Eff, float steep, int A);
 	~Threshold();
 };
