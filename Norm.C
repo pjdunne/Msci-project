d4d18fee7c48e9a4ec329cc44d6302a76e090ef5
alastair95 <32539075+alastair95@users.noreply.github.com>
2018-04-19 16:54:09 +0100

    Update Norm.C

--- Norm.C
+++ Norm.C
@@ -1,13 +1,13 @@
-float NL = 4.6276 * pow(10,7); 
-float NG = 2.995 * pow(10,5);
+float NL = 1720397.562; 
+float NG = 18757.51188;
 
-TH1D* NormL (TH1D h){
+TH1D* NormL (TH1D* h){
 Double_t norm = NL;
 Double_t scale = norm/(h->Integral());
 h->Scale(scale);
 return h;}
 
-TH1D* NormG (TH1D h){
+TH1D* NormG (TH1D* h){
 Double_t norm = NG;
 Double_t scale = norm/(h->Integral());
 h->Scale(scale);
