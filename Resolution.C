516f994f97216f8d6c07285b8383d83a953d5a70
alastair95 <32539075+alastair95@users.noreply.github.com>
2018-05-13 10:45:23 +0100

    Update Resolution.C

--- Resolution.C
+++ Resolution.C
@@ -1,40 +1,44 @@
-#include "Resolution.h"
-#include "TRandom.h"
-Resolution::Resolution()
-{
+//#include "Resolution.h"
+#include "TRandom2.h"
+#include <cmath> 
+#include <random>
+//Resolution::Resolution()
+//{
  
-  float res = 0.05;
+//  float res = 0.1;
 
-}
+//}
 
-vector<Particle> Resolution::ResFunc(vector<Particle> PartVec, float w){
-	TRandom *r0 = new TRandom();
+vector<Particle> ResFunc(vector<Particle> PartVec, float w){
+	TRandom2 *r0 = new TRandom2();
  	float En;
 	vector<Particle> PartDet;
 	PartDet.clear();
 	for(unsigned int i=0;i < PartVec.size();i++){
 		Particle Part = PartVec[i];
-	
-		En=Part.GetEnergy();	
-	        float px=Part.GetPx();
+
+		float px=Part.GetPx();
 		float py=Part.GetPy();
          	float pz=Part.GetPz();
+		float pmag = Part.GetMomentum();
+	     
+		En=Part.GetEnergy();	
+
+		
 
                 Part.SetEnergy(En+(r0->Gaus(0,w*En)));
 		Part.SetPx(px+(r0->Gaus(0,w*px)));
 		Part.SetPx(py+(r0->Gaus(0,w*py)));
 		Part.SetPz(pz+(r0->Gaus(0,w*pz)));
 
-		
-		
-		PartDet.push_back(Part);}			
+		PartDet.push_back(Part);
+		}			
  			
 		
                         					
 return (PartDet);}
 
-Resolution::~Resolution(){}
-
+//Resolution::~Resolution(){}
 
 
 
